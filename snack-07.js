89d9f0f00ab90dc1da01dd8c8fc70aa38b018635
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-23 10:00:59 +0200

    feat snack-07: add function that stops a setInterval after a timeout using clearInterval

--- snack-07.js
+++ snack-07.js
@@ -1,2 +1,17 @@
 // Crea una funzione che ferma un timer dopo un certo tempo:
 // Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+
+function eseguiEferma(message, time_start, time_stop) {
+    return function() {
+        const id = setInterval(() => {
+            console.log(message);
+        }, time_start);
+
+        setTimeout(() => {
+            clearInterval(id)
+        }, time_stop)
+    }
+}
+
+const esegui = eseguiEferma('Sono partito!', 1000, 5000)
+esegui ()
