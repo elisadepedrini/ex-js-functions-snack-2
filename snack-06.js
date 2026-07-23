4f97b78a0b62c35cff877f37937a74f59d71f6d1
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-23 09:39:23 +0200

    feat snack-06: add automatic counter using setInterval and closure

--- snack-06.js
+++ snack-06.js
@@ -1,2 +1,18 @@
 // Crea un contatore automatico con setInterval:
 // Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+
+
+function creaContatoreAutomatico(time_ms) {
+
+    let count = 0
+
+    return function() {
+        setInterval(() => {
+            count++;
+            console.log(count);
+        }, time_ms)
+    }
+}
+
+const countFunction = creaContatoreAutomatico(2000);
+countFunction()
