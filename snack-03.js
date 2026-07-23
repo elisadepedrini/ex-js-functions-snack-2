fa92ec7e27ffacf6fb9d632d35f56335b89dc7f4
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-23 09:07:22 +0200

    feat snack-03: add function with 3 arguments including a callback

--- snack-03.js
+++ snack-03.js
@@ -10,3 +10,12 @@
 
 // console.log(eseguiOperazione(3, 4, somma)); // 7
 // console.log(eseguiOperazione(3, 4, moltiplica)); // 12
+
+
+function eseguiOperazione(num1, num2, callback) {
+    return callback(num1, num2)
+}
+
+const dividi = (num1, num2) => num1 / num2
+
+console.log(eseguiOperazione(6, 2, dividi))
