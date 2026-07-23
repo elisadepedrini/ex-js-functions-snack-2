eac0b155fe1154c28db27d74082110c2d4d212f9
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-23 08:52:49 +0200

    feat snack-02: add arrow function to calculate the square of a number

--- snack-02.js
+++ snack-02.js
@@ -1,2 +1,6 @@
 // Crea una arrow function che calcola il quadrato di un numero:
 // Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+const quadrato = (num) => num * num
+
+console.log(quadrato(6))
