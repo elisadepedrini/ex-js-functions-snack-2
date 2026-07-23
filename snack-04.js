b0b84d53892338b228885c005a0dc19c8b2b3e35
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-23 09:18:48 +0200

    feat snack-04: add function that returns a timer using setTimeout

--- snack-04.js
+++ snack-04.js
@@ -1,2 +1,16 @@
 // Crea un generatore di funzioni creaTimer:
 // Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+
+function creaTimer(time_ms) {
+    console.log('Pronti...');
+    
+    return function() {
+        setTimeout(() => {
+            console.log('Tempo scaduto!'); 
+        }, time_ms)
+    }
+}
+
+const timer = (creaTimer(3000))
+timer()
