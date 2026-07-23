a08728aa2796ae41833d6569f3be50f1ad738952
elisadepedrini <elisa.depedrini@gmail.com>
2026-07-23 09:33:21 +0200

    feat snack-05: add function that prints a message every second using setInterval

--- snack-05.js
+++ snack-05.js
@@ -2,3 +2,11 @@
 // Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
 
 // Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+
+function stampaOgniSecondo(message) {
+    setInterval(() => {
+        console.log(message);
+    }, 1000)
+}
+
+const stampa = stampaOgniSecondo('È passato 1 sec!')
